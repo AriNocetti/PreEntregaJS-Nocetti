76029540ab9475bc83fc7420bf6a71901a1e0481
Ariana <arinocetti@gmail.com>
2024-06-24 20:15:56 -0300

    html, imagenes, carrito más completo

--- main.js
+++ main.js
@@ -1,43 +1,68 @@
-const precios = {
-    maceta: 1400,
-    scrunchie: 850,
-    pulsera: 1150,
+let prodcutos = [
+    {id: 1, nombre:'MacetaRedonda', precio: 3000},
+    {id: 2, nombre:'Maceta3D', precio: 4500},
+    {id: 3, nombre:'PulseraPlanetas', precio: 6500},
+    {id: 4, nombre:'PulseraTriple', precio: 5500},
+    {id: 5, nombre:'ScrunchieVintage', precio: 2500},
+    {id: 6, nombre:'ScrunchieLila', precio: 1500},
+];
+
+let carrito = {
+    productos:[],
+
+    agregarProducto: function(producto) {
+        this.productos.push(producto);
+        alert('${producto.nombre}" ha sido agregado al carrito');
+    },
+
+    revoverProducto: function(id) {
+        let productoIndex = this.productos.findIndex(producto => producto.id === id);
+        if (productoIndex !== -1) {
+            let productoEliminado = this.productos.splice(productoIndex, 1)[0];
+            alert('${productoEliminado.nombre}" ha sido eliminado del carrito');
+        } else {
+            alert('Producto con ID ${id}" no encontrado en el carrito');
+        }
+    },
+
+    mostrar1productos: function() {
+        alert('Productos en el carrito: ');
+        this.productos.forEach(producto => {
+            alert('- ${producto.nombre}: $${producto.precio}"');
+        });
+    },
+
+    calcularTotal: function() {
+        let total = 0;
+        this.productos.forEach(producto => {
+            total += producto.precio;
+        });
+        return total;
+    }
 };
 
-function obtenerCosto(producto) {
-    if (precios[producto] !== undefined) {
-        return precios[producto];
-    } else {
-        return 0;
-    }
-}
+function seleccionarProducto() {
+    alert('Productos disponibles: ');
+    prodcutos.forEach(producto => {
+        alert('ID: ${producto.id} - ${producto.nombre} - Precio: $${producto.precio}"');    
+    });
 
-function seleccionarProductos() {
-    let total = 0;
-    let seleccion = "";
-    let continuar = true;
+    while (true) {
+        let idSeleccionado = parseInt(prompt('Ingrese el ID del producto que desea agregar al carrito (o ingrese 0 para terminar):'));
 
-    while (continuar) {
-        seleccion = prompt("Seleccione un producto! o escriba salir");
-        if (seleccion.toLowerCase() === "salir"){
-            continuar = false;
+        if(idSeleccionado === 0){
+            break;
+        }
+
+        let productoSeleccionado = producto.find(producto => producto.id === idSeleccionado);
+
+        if (productoSeleccionado) {
+            carrito.agregarProducto(productoSeleccionado);
         } else {
-            let costo = obtenerCosto(seleccion.toLowerCase());
-            if (costo > 0) {
-                total += costo;
-                alert ("Has seleccionado una " + seleccion + ". Costo: " + costo + ". Total actual: " + total);
-            } else {
-                alert("Producto no válido. Por favor, seleccione un producto disponible.");
-            }
+            alert('ID de producto no válido. Por favor, ingrese un ID válido.');
         }
     }
-    return total;
 }
 
-function simuladorDeCompras() {
-    alert("Bienvenido al simulador de compras!");
-    let totalFinal = seleccionarProductos();
-    alert("El costo total de su compra es: " + totalFinal + ". Gracias por tu compra!");
-}
 
-simuladorDeCompras();
+
